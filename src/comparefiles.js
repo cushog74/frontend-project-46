3b08a9ac15ad0242341f110ab9020412e6ec8eaa
cushog74 <145211800+cushog74@users.noreply.github.com>
2024-03-01 21:05:24 +0300

    1

--- src/comparefiles.js
+++ src/comparefiles.js
@@ -10,17 +10,29 @@ const getSortedKeys = (obj1, obj2) => {
 
 const compareFiles = (data1, data2) => {
   const keys = getSortedKeys(data1, data2);
-
-  const differences = keys.reduce((acc, key) => {
+  const conditions = keys.filter((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
-
-    if (value1 !== value2) {
-      acc[key] = { value1, value2 };
+    if (_.isEqual(value1, value2)) {
+      return false;
     }
+    return true;
+  });
 
-    return acc;
-  }, {});
+  const differences = conditions.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    if (_.isUndefined(data2[key])) {
+      return { key, value1, status: 'deleted' };
+    }
+    if (_.isUndefined(data1[key])) {
+      return { key, value2, status: 'added' };
+    }
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return { key, children: compareFiles(value1, value2), status: 'nested' };
+    }
+    return { key, value1, value2, status: 'changed' };
+  });
 
   return differences;
 };
