1f85f691f2d693ab9ac8468dfb85b08a617cdf8c
cushog74 <145211800+cushog74@users.noreply.github.com>
2024-03-01 21:10:35 +0300

    1

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -20,30 +20,31 @@ const getStylish = (tree) => {
   const iter = (currentValue, depth = 1) => {
     const currentIndent = getIdent(depth);
     const bracketIndent = getBrackeIndent(depth);
-    const lines = currentValue.flatMap((node) => {
+
+    const lines = currentValue.map((node) => {
       const {
         key, children, status, value1, value2,
       } = node;
-      switch (status) {
-        case 'nested':
-          return `${currentIndent}  ${key}: ${iter(children, depth + 1)}`;
-        case 'deleted':
-          return `${currentIndent}- ${key}: ${stringify(value1, depth + 1)}`;
-        case 'added':
-          return `${currentIndent}+ ${key}: ${stringify(value2, depth + 1)}`;
-        case 'unchanged':
-          return `${currentIndent}  ${key}: ${stringify(value1, depth + 1)}`;
-        case 'changed':
-          return [
-            `${currentIndent}- ${key}: ${stringify(value1, depth + 1)}`,
-            `${currentIndent}+ ${key}: ${stringify(value2, depth + 1)}`,
-          ];
-        default:
-          throw new Error(`Unknown type ${status}.`);
-      }
-    });
-    return ['{', ...lines, `${bracketIndent}}`].join('\n');
+      const value = status === 'nested'
+        ? `${currentIndent}  ${key}: ${iter(children, depth + 1)}`
+        : status === 'deleted'
+          ? `${currentIndent}- ${key}: ${stringify(value1, depth + 1)}`
+          : status === 'added'
+            ? `${currentIndent}+ ${key}: ${stringify(value2, depth + 1)}`
+            : status === 'unchanged'
+              ? `${currentIndent}  ${key}: ${stringify(value1, depth + 1)}`
+              : status === 'changed'
+                ? [
+                  `${currentIndent}- ${key}: ${stringify(value1, depth + 1)}`,
+                  `${currentIndent}+ ${key}: ${stringify(value2, depth + 1)}`,
+                ].join('\n')
+                : throw new Error(`Unknown type ${status}.`);
+      return `${currentIndent}${bracketIndent}${lines.join('\n')}`;
+    }).join('\n');
+
+    return `{${lines}}`;
   };
+
   return iter(tree);
 };
 
