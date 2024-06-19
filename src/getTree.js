888494bea473379b70596d07e2af1f5bca43f429
cushog74 <k.a.anton010205@gmail.com>
2024-06-19 14:41:16 +0300

    111

--- src/getTree.js
+++ src/getTree.js
@@ -1,27 +1,49 @@
 import _ from 'lodash';
 
+const getType = (file1, file2, key) => {
+  if (!Object.hasOwn(file1, key)) {
+    return 'added';
+  }
+  if (!Object.hasOwn(file2, key)) {
+    return 'deleted';
+  }
+  if (_.isObject(file1[key]) && _.isObject(file2[key])) {
+    return 'nested';
+  }
+  if (file1[key] !== file2[key]) {
+    return 'changed';
+  }
+  return 'unchanged';
+};
+
+const getValue = (file1, file2, key, type) => {
+  switch (type) {
+    case 'added':
+      return file2[key];
+    case 'deleted':
+      return file1[key];
+    case 'nested':
+      return сomparisonFile(file1[key], file2[key]);
+    case 'changed':
+      return { oldValue: file1[key], newValue: file2[key] };
+    default:
+      return file1[key];
+  }
+};
+
 const сomparisonFile = (file1, file2) => {
   const keysFile1 = Object.keys(file1);
   const keysFile2 = Object.keys(file2);
   const keys = _.sortBy(_.union(keysFile1, keysFile2));
-  const result = keys.map((key) => {
-    if (_.isObject(file1[key]) && _.isObject(file2[key])) {
-      return { key, value: сomparisonFile(file1[key], file2[key]), type: 'nested' };
-    }
-    if (!Object.hasOwn(file1, key)) {
-      return { key, value: file2[key], type: 'added' };
-    }
-    if (!Object.hasOwn(file2, key)) {
-      return { key, value: file1[key], type: 'deleted' };
-    }
-    if (file1[key] !== file2[key]) {
-      return {
-        key, value: file1[key], type: 'changed', newValue: file2[key],
-      };
-    }
-    return { key, value: file1[key], type: 'unchanged' };
+
+  return keys.map((key) => {
+    const type = getType(file1, file2, key);
+    const value = getValue(file1, file2, key, type);
+
+    return type === 'changed'
+      ? { key, type, ...value }
+      : { key, value, type };
   });
-  return result;
 };
 
 export default сomparisonFile;
