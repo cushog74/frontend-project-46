49971c93d56e0609e83f6f0b19897eb0e6436322
cushog74 <k.a.anton010205@gmail.com>
2024-06-20 00:22:23 +0300

    111

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,7 @@ import genDiff from '../src/index.js';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .version('12.01.0')
   .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
