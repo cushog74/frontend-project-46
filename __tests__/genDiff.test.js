0cfee217e5b46cf28a8825454fdcd4f8a3bd6d53
cushog74 <k.a.anton010205@gmail.com>
2024-06-20 12:24:36 +0300

    111

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,26 +1,36 @@
-import url from 'url';
-import path from 'path';
-import fs from 'node:fs';
-import genDiff from '../src/index.js';
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
+import gendiff from '../src/index.js';
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const getFilePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFilePath(filename), 'utf-8');
+const getString = (data) => String(data).trim();
 
-const expectedStylish = readFile('resultStylish.txt');
-const expectedPlain = readFile('resultPlain.txt');
-const expectedJson = readFile('resultJson.txt');
+const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
+const readFile = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8');
 
-test.each([
-  ['stylish', 'two json files', 'file1.json', 'file2.json', expectedStylish],
-  ['stylish', 'two yml files', 'filepath1.yml', 'filepath2.yml', expectedStylish],
-  ['stylish', 'yaml and json files', 'filepath1.yaml', 'file2.json', expectedStylish],
-  ['plain', 'two json files', 'file1.json', 'file2.json', expectedPlain],
-  ['plain', 'two yml files', 'filepath1.yml', 'filepath2.yml', expectedPlain],
-  ['json', 'two json files', 'file1.json', 'file2.json', expectedJson],
-  ['json', 'two yml files', 'filepath1.yml', 'filepath2.yml', expectedJson],
-])('%s comparsion %s', (format, nameTest, file1, file2, expected) => {
-  expect(genDiff(getFilePath(file1), getFilePath(file2), format)).toBe(expected);
+const files = [['filepath1.json', 'filepath2.json'], ['filepath1.yaml', 'filepath2.yaml'], ['filepath1.yml', 'filepath2.yml']];
+
+test.each(files)('gendiff for "stylish" format', (file1, file2) => {
+  const filepath1 = getFixturePath(file1);
+  const filepath2 = getFixturePath(file2);
+  const result = readFile('resultstylish.txt');
+  expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(getString(result));
+});
+
+test.each(files)('gendiff for "plain" format', (file1, file2) => {
+  const filepath1 = getFixturePath(file1);
+  const filepath2 = getFixturePath(file2);
+  const result = readFile('resultplain.txt');
+  expect(gendiff(filepath1, filepath2, 'plain')).toEqual(getString(result));
+});
+
+test.each(files)('gendiff for "json" format', (file1, file2) => {
+  const filepath1 = getFixturePath(file1);
+  const filepath2 = getFixturePath(file2);
+  const result = readFile('resultjson.txt');
+  expect(getString(gendiff(filepath1, filepath2, 'json'))).toEqual(getString(result));
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import genDiff from '../src/index.js';
+import { Command } from 'commander';
+import gendiff from '../src/index.js';
+
+const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('-v, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2, options) => {
-    console.log(genDiff(filepath1, filepath2, options.format));
+  .action((filepath1, filepath2) => {
+    const options = program.opts().format;
+    const result = gendiff(filepath1, filepath2, options);
+    console.log(result);
   });
-
+  
 program.parse();
