d2e212d18238b30b368def76b5b7c3c22a363243
cushog74 <k.a.anton010205@gmail.com>
2024-06-20 14:01:54 +0300

    111

--- src/comparefiles.js
+++ src/comparefiles.js
@@ -1,27 +1,35 @@
 import _ from 'lodash';
 
-const сomparisonFile = (file1, file2) => {
-  const keysFile1 = Object.keys(file1);
-  const keysFile2 = Object.keys(file2);
-  const keys = _.sortBy(_.union(keysFile1, keysFile2));
-  const result = keys.map((key) => {
-    if (_.isObject(file1[key]) && _.isObject(file2[key])) {
-      return { key, value: сomparisonFile(file1[key], file2[key]), type: 'nested' };
-    }
-    if (!Object.hasOwn(file1, key)) {
-      return { key, value: file2[key], type: 'added' };
-    }
-    if (!Object.hasOwn(file2, key)) {
-      return { key, value: file1[key], type: 'deleted' };
-    }
-    if (file1[key] !== file2[key]) {
-      return {
-        key, value: file1[key], type: 'changed', newValue: file2[key],
-      };
-    }
-    return { key, value: file1[key], type: 'unchanged' };
-  });
-  return result;
+const getSortedKeys = (obj1, obj2) => {
+  const keys1 = _.keys(obj1);
+  const keys2 = _.keys(obj2);
+  const unionKeys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(unionKeys);
+  return sortedKeys;
 };
 
-export default сomparisonFile;
+const compareFiles = (data1, data2) => {
+  const keys = getSortedKeys(data1, data2);
+  const conditions = keys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      return { key, value1, status: 'deleted' };
+    }
+    if (!_.has(data1, key) && _.has(data2, key)) {
+      return { key, value2, status: 'added' };
+    }
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return { key, children: compareFiles(value1, value2), status: 'nested' };
+    }
+    if (_.isEqual(value1, value2)) {
+      return { key, value1, status: 'unchanged' };
+    }
+    return {
+      key, value1, value2, status: 'changed',
+    };
+  });
+  return conditions;
+};
+
+export default compareFiles;
